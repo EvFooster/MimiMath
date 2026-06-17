26c16c2b2bbb9ab9be6633fe810f9f721443a9b9
Fooster <evelinpicachuoutlook@gmail.com>
2026-06-17 20:23:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+let expressaoAtual = '';
+
+function adicionarNumero(numero) {
+    const visor = document.getElementById('visor-fofinho');
+    
+    if (visor.innerText.length >= 12) {
+        return; 
+    }
+    
+  
+    if (visor.innerText === 'Erro') {
+        visor.innerText = '';
+        expressaoAtual = '';
+        visor.style.backgroundColor = '#fff0f3';
+    }
+    
+    if (visor.innerText === '0') {
+        visor.innerText = '';
+    }
+    
+    visor.innerText += numero;
+    expressaoAtual += numero;
+}
+
+
+function definirOperacao(operador) {
+    const visor = document.getElementById('visor-fofinho');
+    
+    if (visor.innerText === 'Erro') {
+        return; 
+    }
+    
+    let sinalParaOTela = operador;
+    if (operador === '*') sinalParaOTela = 'x';
+    if (operador === '/') sinalParaOTela = '÷';
+    
+    visor.innerText += ' ' + sinalParaOTela + ' '; 
+    expressaoAtual += operador; 
+}
+
+function limparVisor() {
+    const visor = document.getElementById('visor-fofinho');
+    visor.innerText = '0';
+    expressaoAtual = '';
+    visor.style.backgroundColor = '#fff0f3'; 
+}
+
+
+function calcularResultado() {
+    const visor = document.getElementById('visor-fofinho');
+    try {
+        let resultado = eval(expressaoAtual);
+        visor.innerText = resultado;
+        expressaoAtual = resultado.toString(); 
+    } catch (Invalid) {
+        visor.innerText = 'Invalid';
+        expressaoAtual = '';
+        
+       
+        visor.style.backgroundColor = '#ffdde2'; 
+    }
+}
